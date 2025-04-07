b9fa6789121b8bdfebf01ae4113b71394cdc1657
Diomar Majano <http.diomar@gmail.com>
2025-04-07 10:02:34 -0400

    Creacion de proyecto

---
+++ src/main/java/pelis/cloud/peliscloudjpa/PeliscloudjpaApplication.java
@@ -0,0 +1,13 @@
+package pelis.cloud.peliscloudjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeliscloudjpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeliscloudjpaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pelis/cloud/peliscloudjpa/PeliscloudjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package pelis.cloud.peliscloudjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeliscloudjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
